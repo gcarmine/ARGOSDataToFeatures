a75df23b96813e0390473ec4a24001d67993f516
gcarmine <gabbycarmine@gmail.com>
2020-10-20 16:00:27 -0400

    Creates empty feature class

--- importARGOS.py
+++ importARGOS.py
@@ -7,25 +7,32 @@
 ## Usage: ImportArgos <ARGOS folder> <Output feature class> 
 ##
 ## Created: Fall 2020
-## Author: gabrielle.carmine@duke.edu (for ENV859)
+## Author: John.Fay@duke.edu (for ENV859)
 ##---------------------------------------------------------------------
 
 # Import modules
 import sys, os, arcpy
 
+#Allow arcpy to overwrite outputs
+arcpy.env.overwriteOutput = True
+
 # Set input variables (Hard-wired)
-inputFile = 'V:/ARGOSTracking/Data/ARGOSData/1997dg.txt'
+inputFile = 'V:\\ARGOSTracking\\Data\\ARGOSData\\1997dg.txt'
 outputFC = "V:/ARGOSTracking/Scratch/ARGOStrack.shp"
+
+#Create an empty feature class to which we'll add features
+outPath, outName = os.path.split(outputFC)
+arcpy.CreateFeatureclass_management(outPath,outName, "POINT")
 
 #%% Construct a while loop to iterate through all lines in the datafile
 # Open the ARGOS data file for reading
 inputFileObj = open(inputFile,'r')
 
-# Get the first line of data, so we can use a while loop
+# Get the first line of data, so we can use the while loop
 lineString = inputFileObj.readline()
 
-# Start the while loop
-while lineString:
+#Start the while loop
+while lineString: 
     
     # Set code to run only if the line contains the string "Date: "
     if ("Date :" in lineString):
@@ -35,6 +42,9 @@ while lineString:
         
         # Extract attributes from the datum header line
         tagID = lineData[0]
+        obsDate= lineData[3]
+        obsTime = lineData[4]
+        obsLC = lineData[7]
         
         # Extract location info from the next line
         line2String = inputFileObj.readline()
@@ -47,7 +57,7 @@ while lineString:
         obsLon= line2Data[5]
         
         # Print results to see how we're doing
-        print (tagID,"Lat:"+obsLat,"Long:"+obsLon)
+        print (tagID,obsDate,obsTime,obsLC,"Lat:"+obsLat,"Long:"+obsLon)
         
     # Move to the next line so the while loop progresses
     lineString = inputFileObj.readline()
